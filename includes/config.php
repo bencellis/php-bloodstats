529c6b8817b7e83aaee42c5dcf8c2469b098d09c
Benjamin Ellis <benjamin.c.ellis@gmail.com>
2020-10-27 09:12:30 +0000

    initial commit

---
+++ includes/config.php
@@ -0,0 +1,33 @@
+<?php
+
+// database configuration
+$dbconfig = array(
+    'dbserver' => 'localhost',
+    'dbuser' => 'benjamin',
+    'dbpasswd' => '5hamwar1',
+    'dbname' => 'bloodstats',
+    'dbport' => 3306
+);
+
+// config file
+$ganalyticsid = '';
+$adsenseid = '';
+$amazonids = array(
+	'co.uk'	=> '',
+	'com'	=> '',
+);
+$keywords = array();
+$title = 'Blood Statistics';
+$welcomemsg = 'Welcome';
+$author = 'Benjamin Ellis, Mukudu Sites, Mukudu Ltd';
+$description = "Benjamin's blood statistics.";
+
+//cookie consent
+$theme = "light-top";		// for dark sites
+//$theme = "dark-top";			// for light sites
+$message = "This website uses cookies to ensure you get the best experience on our website";
+$dismissmsg = "Got it!";
+$moremsg = "More info";
+$morelink = '/cookiepolicy.php';		// link to policy document
+
+
